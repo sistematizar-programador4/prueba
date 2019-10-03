7d17ce26d4010e9d6c6de63c2978d8bb1eb0fa26
midas <midas@midas.midas>
2019-10-03 10:15:22 -0500

    Entrega

--- app/serializers.py
+++ app/serializers.py
@@ -13,6 +13,8 @@ class ProfessorSerializer(serializers.ModelSerializer):
         fields = ('id', 'name')
 
 class ScoreSerializer(serializers.ModelSerializer):
+    professor = serializers.PrimaryKeyRelatedField(many=False,read_only=True)
+    student = serializers.PrimaryKeyRelatedField(many=False,read_only=True)
     class Meta:
         model = Score
-        fields = ('id', 'name', 'value', 'student_id', 'professor_id')
+        fields = ('id', 'name', 'value', 'student_id', 'professor_id','professor','student')
